cffca43b31f3fcd21332f704c4521e4f1611383f
Your Name <markprotsyuk@Marks-MacBook-Pro-2.local>
2022-09-19 10:30:43 -0700

    added a lot of things and changes

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,70 @@
-import React from "react";
+import { useState } from "react";
+import { useRouter } from "next/router";
+import Auth from "../utils/auth";
+import { validateEmail } from "../utils/helpers";
 
-export default function Auth() {
-  return <div>auth</div>;
+export default function Login() {
+  const [message, setMessage] = useState("");
+  const router = useRouter();
+  const submitForm = (e) => {
+    e.preventDefault();
+    const formDate = new FormData();
+    let values = {};
+    router.push({
+      pathname: "/chat",
+      query: values,
+    });
+  };
+
+  const signUpForm = (e) => {
+    router.push({
+      pathname: "/signup",
+    });
+  };
+
+  return (
+    <div className="bg-indigo-100 min-h-screen flex flex-col items-center place-content-center block">
+      <div className="text-center text-sky-700 text-8xl shadow-xl mb-10 p-5 rounded-full">
+        Talky-Talky 📱
+      </div>
+      <div>
+        <form
+          onSubmit={(e) => submitForm(e)}
+          className="flex-col border border-black rounded px-8 py-3 bg-slate-200 shadow-xl"
+        >
+          <div className="flex justify-between flex-col">
+            <input
+              className="mb-3 p-2 border border-black rounded hover:text-indigo-300"
+              placeholder="Email"
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <input
+              className="p-2 border border-black rounded hover:text-indigo-300"
+              type="password"
+              placeholder="Password"
+              onChange={(e) => setSecret(e.target.value)}
+            />
+            <h2 className="p-2 my-3 text-xl">{message}</h2>
+          </div>
+          <div className="flex flex-col place-content-center">
+            <button
+              type="submit"
+              className="p-2 mb-3 border bg-white text-sky-700 border-black rounded hover:bg-sky-700 hover:text-white"
+            >
+              Login
+            </button>
+          </div>
+        </form>
+        <div className="flex justify-center">
+          <button
+            onClickz={(e) => signUpForm(e)}
+            type="submit"
+            className="p-2 border text-sky-700 hover:text-white"
+          >
+            Don't have an account? Sign up!
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
---
+++ utils/helpers.js
@@ -0,0 +1,14 @@
+export function validateEmail(email) {
+  const re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
+export function checkPassword(input) {
+  const passw = /^[A-Za-z.\s_-]\w{5,20}$/g;
+
+  if (input.match(passw)) {
+    return true;
+  }
+  return false;
+}
